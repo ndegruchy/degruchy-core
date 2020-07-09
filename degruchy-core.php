47bec43561639fb459fb0c0a1cd4eda55c0edec1
Nathan DeGruchy <nathan@degruchy.org>
2020-07-09 14:49:39 -0400

    Cleanup of CSP headers

--- degruchy-core.php
+++ degruchy-core.php
@@ -27,7 +27,57 @@
  */
 function degruchy_csp()
 {
-	header("Content-Security-Policy: default-src 'self' www.degruchy.org; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data: https://cdn.shortpixel.ai; upgrade-insecure-requests; block-all-mixed-content; base-uri 'self'; report-uri https://degruchy.report-uri.com/r/d/csp/enforce");
+	// Settings matrix
+	$csp_options = array(
+		"default-src" => array(
+			"'self'",
+			"https://www.degruchy.org",
+		),
+		"base-uri" => array(
+			"'self'",
+			"https://www.degruchy.org"
+		),
+		"script-src" => array(
+			"'self'",
+			"'unsafe-inline'",
+		),
+		"style-src" => array(
+			"'self'",
+			"'unsafe-inline'",
+		),
+		"font-src" => array(
+			"'self'",
+			"data:",
+		),
+		"img-src" => array(
+			"'self'",
+			"data:",
+			"https://cdn.shortpixel.ai"
+		),
+		"report-uri" => "https://degruchy.report-uri.com/r/d/csp/enforce",
+	);
+
+	$csp_string = ""; // Empty by default
+
+	foreach ($csp_options as $rule => $setting) {
+		// For each item in the top-level array
+		if (is_array($setting)) {
+			// If we find the value is another array, loop in
+			$csp_string .= $rule; // First part
+			foreach ($setting as $item) {
+				$csp_string .= " " . $item; // Append setting
+			}
+		} else {
+			// If it's just a simple k=>v, then add it as per normal.
+			$csp_string .= $rule . " " . $setting;
+		}
+		$csp_string .= "; "; // separator
+	}
+
+	$csp_string .= " upgrade-insecure-requests; block-all-mixed-content;"; // non-value rules
+
+	header("Content-Security-Policy: $csp_string"); // send!
+
 	return true;
 }
 
@@ -39,30 +89,31 @@ add_filter( 'send_headers', 'degruchy_csp' );
  * so on sites with large postmeta tables it is super slow
  * and is rarely useful anymore on any site
  */
-function s9_remove_post_custom_fields_metabox()
+function degruchy_custom_fields_metabox()
 {
-    foreach ( get_post_types( '', 'names' ) as $post_type )
+	foreach ( get_post_types( '', 'names' ) as $post_type )
 	{
-         remove_meta_box( 'postcustom' , $post_type , 'normal' );
-    }
+		remove_meta_box( 'postcustom' , $post_type , 'normal' );
+	}
 	return true;
 }
-add_action( 'admin_menu' , 's9_remove_post_custom_fields_metabox' ); 
+add_action( 'admin_menu' , 'degruchy_custom_fields_metabox' ); 
 
 /**
  * Display Old Post Notification on Content
  */
-function display_old_post_notification_on_content( $content )
+function degruchy_old_post_notification_on_content( $content )
 {
+	// TODO: Factor out banner into template?
 	if( !is_singular( 'post' ) )
 		return $content;
-	
+
 	$postd  = get_the_date( 'U' );
 	$today  = date( 'U' );
 	$oneyr  = 60 * 60 * 24 * 365;
 	$cats   = get_categories();
 	$show   = 0;
-	$banner = '<section id="old"> <p> <em><strong>Please Note:</strong> This post is a year old or older. That means the content may have changed, <a href="https://degruchy.org/notes/evolving-thoughts/">be out of date with current thinking or just plain wrong</a>. If you have any questions, comments or issues regarding this content, please <a href="https://degruchy.org/about-me/">send me an email</a> with the link and your message.</em> </p> <p> <em>Thank you.</em> </p> <p> <em>-- Nathan</em> </p> </section>';
+	$banner = '<section id="old"><p><em><strong>Please Note:</strong> This post is a year old or older. That means the content may have changed, <a href="https://degruchy.org/notes/evolving-thoughts/">be out of date with current thinking or just plain wrong</a>. If you have any questions, comments or issues regarding this content, please <a href="https://degruchy.org/about-me/">send me an email</a> with the link and your message.</em></p><p><em>Thank you.</em></p><p><em>&mdash; Nathan</em></p></section>';
 
 	foreach ( $cats as $category )
 	{ // Loop through the assigned categories
@@ -87,11 +138,11 @@ function display_old_post_notification_on_content( $content )
 	}
 }
 
-add_filter( 'the_content', 'display_old_post_notification_on_content', 99 );
+add_filter( 'the_content', 'degruchy_old_post_notification_on_content', 99 );
 
 function degruchy_mime_types( $mimes )
 {
- 	// $mimes[ 'svg'  ] = 'image/svg+xml'; // doesn't seem to work...
+	// $mimes[ 'svg'  ] = 'image/svg+xml'; // doesn't seem to work...
 	$mimes[ 'svg'  ] = 'image/svg';
 	$mimes[ 'webp' ] = 'image/webp';
 	$mimes[ 'webm' ] = 'video/webm';
